e86e0fcdc8cac0bab99b2ce4af3f16082ecfca8c
GeralRios <geralrios750@gmail.com>
2023-10-06 10:44:34 -0500

    ayuudaaaa555558888888

--- src/main/java/com/mycompany/vista/FrmBusquedaResidente.java
+++ src/main/java/com/mycompany/vista/FrmBusquedaResidente.java
@@ -409,7 +409,7 @@ public class FrmBusquedaResidente extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
+        //</editor-fold> .
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
